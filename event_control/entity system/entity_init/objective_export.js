a235b73310bcadc3d2c4dbe949aae2dd86ffa221
DeMisterio <denilarsanov33@gmail.com>
2025-12-07 02:44:21 +0000

    entity exporter v1

--- event_control/entity system/entity_init/objective_export.js
+++ event_control/entity system/entity_init/objective_export.js
@@ -5,71 +5,116 @@ export const CHARACTERdata = JSON.parse("event_control/entity system/entities/Ch
 export const ITEMSdata = JSON.parse("event_control/entity system/entities/items/items.json", "utf-8")
 
 
-export class scenes{
-    constructor(scene_n, scene_id){
-        this.scene_id = scene_id
-        this.scene_n = scene_n
-        this.scene_title = SCENEdata.scenes.find(st => st.scene_n === scene_n).scene_title
-        this.scene_locations = SCENEdata.scenes.find(l => l.scene_id == scene_id).locations
-        this.scene_texts = SCENEdata.scenes.find(t => t.scene_id = scene_id).intro_text
+export class scenes {
+    constructor(scene_n, scene_id = null) {
+        let scene = null;
+        if (scene_id !== null) {
+            scene = SCENEdata.scenes.find(st => st.scene_id === scene_id);
+        } else {
+            scene = SCENEdata.scenes.find(st => st.scene_n === scene_n);
+        }
+        if (!scene) {
+            throw new Error(`Scene not found: scene_n=${scene_n}, scene_id=${scene_id}`);
+        }
+        this.scene_id = scene.scene_id;
+        this.scene_n = scene.scene_n;
+        this.scene_title = scene.scene_title;
+        this.scene_locations = scene.locations;
+        this.scene_texts = scene.intro_text;
     }
 
+    getNextScene() {
+        const next = SCENEdata.scenes.find(st => st.scene_n === this.scene_n + 1);
+        if (!next) return null;
+        return new scenes(next.scene_n, next.scene_id);
+    }
 }
 export class Character {
     #name;
-    #character_type
-    #personality
-    #init_parameters
+    #character_type;
+    #personality;
+    #init_parameters;
+
     constructor(preseted = null, name = null, character_type = null, personality = null, init_parameters = null) {
         this.preseted = preseted;
-        this.#name = name;
-        this.#character_type = character_type
-        this.#personality = personality
-        this.#init_parameters = init_parameters
-        this.strength = this.#init_parameters[3]["strength"]
-        this.init_items = this.#init_parameters[4]["init_items"]
-        this.health = this.#init_parameters[0]["health"]
-        this.action_speed = this.#init_parameters[1]["action_speed"]
+
+        if (this.preseted !== null) {
+            const presetData = CHARACTERdata.characters.find(ch => ch.character_n === this.preseted);
+            if (!presetData) {
+                throw new Error(`Character preset not found: ${this.preseted}`);
+            }
+            this.#name = presetData.character_n;
+            this.#character_type = presetData.character_type;
+            this.#personality = presetData.personality;
+            this.#init_parameters = presetData.init_parameters;
+        } else {
+            this.#name = name;
+            this.#character_type = character_type;
+            this.#personality = personality;
+            this.#init_parameters = init_parameters || [];
+        }
+        this.health = this.#init_parameters[0]?.health ?? null;
+        this.action_speed = this.#init_parameters[1]?.action_speed ?? null;
+        this.endurance = this.#init_parameters[2]?.endurance ?? null;
+        this.strength = this.#init_parameters[3]?.strength ?? null;
+        this.init_items = this.#init_parameters[4]?.init_items ?? [];
     }
     get name() {
-        if (this.preseted === null) {
-            return this.#name;
-        } else {
-            return this.preseted.name;
-        }
+        return this.#name;
     }
     get character_type() {
-        if (this.preseted === null) {
-            return this.#character_type;
-        } else {
-           return CHARACTERdata.characters.find(ch_t => ch_t.character_n === this.name).character_type
-        }
+        return this.#character_type;
     }
-    get personality() {
-        if (this.preseted === null) {
-            return this.#personality;
-        } else {
-            return CHARACTERdata.characters.find(ch_t => ch_t.character_n === this.name).personality
 
-        }
+    get personality() {
+        return this.#personality;
     }
+
     get init_parameters() {
-        if (this.preseted === null) {
-            return this.#init_parameters;
-        } else {
-            return CHARACTERdata.characters.find(ch_t => ch_t.character_n === this.name).init_parameters
-        }
+        return this.#init_parameters;
     }
 }
 
-export class location extends scenes{
-    constructor(location_id){
-        super(scene_locations)
-        this.location_id = 
-        this.scene_n = scene_n
-        this.scene_title = SCENEdata.scenes.find(st => st.scene_n === scene_n).scene_title
-        this.scene_locations = SCENEdata.scenes.find(l => l.scene_id == scene_id).locations
-        this.scene_texts = SCENEdata.scenes.find(t => t.scene_id = scene_id).intro_text
+export class location {
+    constructor(location_id) {
+        const loc = LOCATIONdata.locations.find(l => l.location_id === location_id);
+        if (!loc) {
+            throw new Error(`Location not found: ${location_id}`);
+        }
+
+        this.location_id = loc.location_id;
+        this.location_n = loc.location_n;
+        this.sub_locations = loc.sub_locations;      // массив саб-локаций
+        this.scene_id = loc.scene_id ?? null;
+
+        // персонажи как объекты класса Character
+        this.characters = (loc.characters || []).map(name => new Character(name));
+
+        // пока просто список id предметов в этой локации (если тебе нужно детальнее - можно расширить)
+        this.items = this._loadItemsFromSubLocations();
     }
 
+    _loadItemsFromSubLocations() {
+        // здесь заглушка, так как точной структуры sets/containers ты не дал в этом файле
+        // можно позже связать с отдельным модулем sets/containers
+        return [];
+    }
+}
+
+export class Item {
+    constructor(item_id) {
+        const data = ITEMSdata.items.find(it => it.id === item_id);
+        if (!data) {
+            throw new Error(`Item not found: ${item_id}`);
+        }
+
+        this.id = data.id;
+        this.type = data.type;
+        this.NV = data.NV ?? null;
+        this.Eating_speed = data.Eating_speed ?? null;
+        this.effect = data.effect ?? null;
+        this.functionality = data.functionality ?? null;
+        this.HarmRate = data.HarmRate ?? null;
+        this.movable = data.movable || { access: false, weight: 0 };
+    }
 }
