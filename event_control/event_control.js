e7cdb95708679acd58575c58e9fee82cf688eb38
DeMisterio <denilarsanov33@gmail.com>
2025-12-16 00:57:16 +0000

    bug fixes

--- event_control/event_control.js
+++ event_control/event_control.js
@@ -2,7 +2,7 @@
 
 import { character, location, item, scenes, loadJSONData } from './entity_system/entity_init/objective_export.js'
 import { send_text, get_text } from '../effect_control.js'
-import { HelloWorld as DisplayIntroText } from '../script.js'
+
 // The game initialisation flow: 
 
 // 0. Check the logs for savings if no - start location => scene[0]
@@ -24,9 +24,51 @@ export class GameState {
 // Преобразуем список имён в список персонажей
 
 
+const INTRO_TEXT = [
+    "Welcome.",
+    "Take a breath.",
+    "This is a story-driven game.",
+    "And to play it,",
+    "you can just type what you want to do.",
+    "Simple words are enough.",
+    "If it sounds human — it usually works. No need to type 'Go north', or 'Go south'",
+    "Be creative, and use a power of your language.",
+    "There is no perfect way to play.",
+    "You don’t need to guess the right command.",
+    "Just say what feels natural.",
+    "For example: Go to the kitchen / Pick up the passport / Leave the house",
+    "You are not a hero here.",
+    "You are just a person in a bad situation.",
+    "Trying to make it through.",
+    "The world outside is unstable.",
+    "People are scared.",
+    "Things happen fast.",
+    "Some choices matter.",
+    "Some mistakes are okay.",
+    "The game will adapt to you.",
+    "You can finish this game in about 10 minutes.",
+    "But you can also slow down and explore.",
+    "The game won’t always guide you.",
+    "But it will try to understand you.",
+    "You can skip the intro textes by pressing the space key, but it will not give you any privilege except the time, thich is what really MATTERS.",
+    "Be yourself.",
+    "Be human.",
+    "Be the last of us.",
+    "Let’s begin.",
+    "",
+    "",
+    " "
+];
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function DisplayIntroText() {
+    for (const line of INTRO_TEXT) {
+        outputText(line);
+        await sleep(1676);
+    }
 }
 
 async function out_scene_text() {
